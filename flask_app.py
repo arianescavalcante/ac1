0b96893a4fe1a57b94b1e15bc0f69f9d452f52aa
ariane.cavalcante@alun.faculdadeimpacta.com.br <ariane.cavalcante@aluno.faculdadeimpacta.com.br>
2020-03-12 01:21:08 +0000

    Pasta toda

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,16 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def hello_world ():
+    return '<h1>Meu Primeiro site </h1>'
 
+
+
+@app.route('/home')
+def inicio ():
+    return render_template("inicio.html")
